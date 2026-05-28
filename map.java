7ccd574f49effdb86d41028b3ce56e64066cd911
powershot6615-sys <powershot6615@gmail.com>
2026-05-29 05:54:35 +0900

    완성

--- map.java
+++ map.java
@@ -1,6 +1,18 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        return answer;
-    }
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+       String[] answer = new String[n];
+      
+          for(int i=0;i<n;i++) {
+              int num = arr1[i]|arr2[i];
+              //합친 2진수 지도값 넣기
+              String b = Integer.toBinaryString(num);
+              //문자열 합치기
+              b=String.format("%" + n + "s",b);
+              // 공백과 #으로 변경
+              answer[a]=b.replace("0"," ").replace("1", "#");
+              
+          }
+          return answer;
+  }
 }
+        
